e2efc3df0265bd0d3dc98b1c69c3f544b46876fb
Jayant Gulati <jayantgulati5@gmail.com>
2025-11-10 18:34:34 +0530

    Updated the Experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,32 +1,38 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Cloud Security Engineer',
-    company: " Aquanow ",
-    duration: "(Dec 2024 - Present)"
+    title: "Cloud Security Architect",
+    company: " Wisdom Square Technologies ",
+    duration: "(July 2025 - Present)",
   },
   {
     id: 2,
-    title: "Deputy Manager - Cloud Security",
-    company: "Jubilant FoodWorks Private limited, Noida",
-    duration: "(Sep 2022 - Dec 2024)"
+    title: "Cloud Security Engineer",
+    company: " Aquanow ",
+    duration: "(Dec 2024 - July 2025)",
   },
   {
     id: 3,
-    title: "AWS Security Engineer",
-    company: "Absolutdata Solutions Pvt Ltd, Gurgaon",
-    duration: "(Sep 2021 - Sep 2022)"
+    title: "Deputy Manager - Cloud Security",
+    company: "Jubilant FoodWorks Private limited, Noida",
+    duration: "(Sep 2022 - Dec 2024)",
   },
   {
     id: 4,
-    title: "Associate",
-    company: "SoftwareONE Pvt Ltd, Gurgaon",
-    duration: "(Apr 2020 - Sep 2021)"
+    title: "AWS Security Engineer",
+    company: "Absolutdata Solutions Pvt Ltd, Gurgaon",
+    duration: "(Sep 2021 - Sep 2022)",
   },
   {
     id: 5,
+    title: "Associate",
+    company: "SoftwareONE Pvt Ltd, Gurgaon",
+    duration: "(Apr 2020 - Sep 2021)",
+  },
+  {
+    id: 6,
     title: "Associate Professional",
     company: "DXC Technology Pvt Ltd, Noida",
-    duration: "(Sep 2018 - Apr 2020)"
-  }
-]
+    duration: "(Sep 2018 - Apr 2020)",
+  },
+];
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
   name: "Jayant GULATI",
-  profile: '/profile.svg',
+  profile: "/profile.svg",
   designation: "Cloud Security Engineer",
-  description: "My name is JAYANT GULATI. I am a Cloud Security Engineer with 6 years of experience in securing cloud environments, specializing in AWS security, IAM governance, and compliance automation. I design and implement robust security controls, leveraging Terraform and Python to enhance cloud security posture. With expertise in threat detection, identity management, and compliance frameworks, I help organizations strengthen their cloud defenses. Passionate about cybersecurity, I stay ahead of emerging threats to ensure resilient and scalable cloud security solutions.",
-  email: 'jayantgulati5@gmail.com',
-  phone: '',
-  address: '',
-  github: 'https://github.com/jayantgulati',
-  facebook: '',
-  linkedIn: 'https://www.linkedin.com/in/%E2%98%81%EF%B8%8Fjayant-gulati-2b7350b5/',
-  twitter: '',
-  stackOverflow: '',
+  description:
+    "My name is JAYANT GULATI. I am a Cloud Security Engineer with 6 years of experience in securing cloud environments, specializing in AWS security, IAM governance, and compliance automation. I design and implement robust security controls, leveraging Terraform and Python to enhance cloud security posture. With expertise in threat detection, identity management, and compliance frameworks, I help organizations strengthen their cloud defenses. Passionate about cybersecurity, I stay ahead of emerging threats to ensure resilient and scalable cloud security solutions.",
+  email: "jayantgulati5@gmail.com",
+  phone: "",
+  address: "",
+  github: "https://github.com/jayantgulati",
+  facebook: "",
+  linkedIn:
+    "https://www.linkedin.com/in/%E2%98%81%EF%B8%8Fjayant-gulati-2b7350b5/",
+  twitter: "",
+  stackOverflow: "",
   leetcode: "",
   devUsername: "",
-  resume: "googledrivelink"
-}
+  resume: "googledrivelink",
+};
